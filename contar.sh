e6d2d827db1cacc6bd62a2ab0ea481717bc27558
Federico-xaca <fede.preno@gmail.com>
2023-04-27 14:04:33 -0300

    update contar.sh

--- contar.sh
+++ contar.sh
@@ -1,7 +1,16 @@
-for i in {1..5}
-do 
+contador=0
+     for i in {1..5}
+            do 
+                lineas=$(wc -l < loremipsum-${i}.txt) 
+                contador=$(($contador+$lineas))
+                echo "El archivo loremipsum-${i}.txt tiene $lineas lineas."
 
- LINEAS=$(wc -l < loremipsum-${i}.txt) 
-  
-  echo "El archivo loremipsum-${i}.txt tiene $LINEAS lineas."
-done
+    done
+
+if [ -f Sumatoria.txt ];
+then
+echo "Las linas totales son $contador."
+else
+echo "El total de lineas entre los archivos es de: $contador lineas." >> Sumatoria.txt 
+echo "Las linas totales son $contador."
+fi
